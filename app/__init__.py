c7213efd06413cef2fc7455792da742c2910fd52
Thomas Wissemann <tcwissemann@gmail.com>
2024-08-22 21:29:55 -0500

    small changes
    - improved config
    - cleaned up create_app
    - fixed displaced div upon flash alert

--- app/__init__.py
+++ app/__init__.py
@@ -3,37 +3,33 @@ from flask import Flask, flash, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
 from flask_mail import Mail
-
+from app.config import DevelopmentConfig
 
 app = Flask(__name__)
-app.config.from_object('app.config')
-
+app.config.from_object(DevelopmentConfig)
 db = SQLAlchemy()
-DB_NAME = "app.db"
 mail = Mail(app)
 
 def create_app():
-    #should be better than this
-    database_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'database')
-
     #Ensure the database directory exists
+    database_path = app.config['DATABASE_PATH']
     if not os.path.exists(database_path):
         os.makedirs(database_path)
 
-    app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{os.path.join(database_path, DB_NAME)}'
     db.init_app(app)
 
-    from .views import views
-    from .auth import auth
-
-    app.register_blueprint(views, url_prefix='/')
-    app.register_blueprint(auth, url_prefix='/')
-
+    #create models
     from .models import User, Keep
-
     with app.app_context():
         db.create_all()
 
+    #Import/Register Blueprints
+    from .views import views
+    from .auth import auth
+    app.register_blueprint(views, url_prefix='/')
+    app.register_blueprint(auth, url_prefix='/')
+
+    #login manager
     login_manager = LoginManager()
     login_manager.login_view = 'auth.login'
     login_manager.init_app(app)
