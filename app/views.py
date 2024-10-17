36789f614a29ba04dacdc8de143880c6575a530b
Thomas Wissemann <tcwissemann@gmail.com>
2024-10-17 17:19:12 -0500

    Update views.py

--- app/views.py
+++ app/views.py
@@ -15,25 +15,9 @@ def home():
 @views.route('/dashboard', methods=['GET', 'POST'])
 @login_required
 def dashboard():
-    nations = get_user_nations()
+    nations = Nation.query.filter_by(user_id=current_user.id).all()
     remaining_slots = (5-(len(nations)))
     form = nation_form_init()
-    if request.method == 'POST':
-        if form.validate_on_submit():
-            try:
-                create_nation(form)
-                flash(f'Success, {form.nation_name.data} was added', category='success')
-            except Exception as e:
-                flash(f'failure, {form.nation_name.data} was not added error: {e}', category='error')
-                print(form.data)
-        else:
-             # Print out each field's errors
-            for field, errors in form.errors.items():
-                for error in errors:
-                    print(f"Error in {getattr(form, field).label.text}: {error}")
-            flash('Invalid Nation submission', category='error')
-            return redirect(url_for('views.dashboard'))
-
     return render_template("core/dashboard.html", form=form, user=current_user, nations=nations, remaining_slots=remaining_slots)
 
 #modes endpoint for dependent select
@@ -51,14 +35,16 @@ def get_modes():
 
     return render_template("core/select_mode_response.html", modes=modes)
 
-def create_nation(form):
-    nation_exists = Nation.query.filter_by(nation_name=form.nation_name.data).first()
-    if nation_exists:
-        flash('A nation with this name already exists in NSRegent!', category='error')
-    else:
-        new_nation = Nation(nation_name=form.nation_name.data, nation_password=form.nation_password.data, service_type=form.service_type.data, mode=form.mode.data, user_id=current_user.id)
-        db.session.add(new_nation)
-        db.session.commit()
+# @views.route('/create_nation', methods=['POST'])
+# @login_required
+# def create_nation(form):
+#     nation_exists = Nation.query.filter_by(nation_name=form.nation_name.data).first()
+#     if nation_exists:
+#         flash('A nation with this name already exists in NSRegent!', category='error')
+#     else:
+#         new_nation = Nation(nation_name=form.nation_name.data, nation_password=form.nation_password.data, service_type=form.service_type.data, mode=form.mode.data, user_id=current_user.id)
+#         db.session.add(new_nation)
+#         db.session.commit()
 
 
 @views.route('/delete_nation', methods=['POST'])
@@ -68,23 +54,29 @@ def delete_nation():
     #htmx to delete the nation without page reload
     pass
 
-def get_user_nations():
-    nations = Nation.query.filter_by(user_id=current_user.id).all()
-    nation_array = []
-    for nation in nations:
-        nation_array.append(DisplayNation(nation.flag, nation.nation_name, nation.service_type, nation.mode, nation.status, nation.date_created))
-    return nation_array
-
 def nation_form_init():
     form = NationForm()
     form.mode.choices = [(mode.id, mode.name) for mode in ServiceMode.query.all()]
     form.mode.choices.insert(0, (0, 'Select Service First'))
     return form
-class DisplayNation:
-    def __init__(self, flag, nation_name, service_type, mode, status, date_created):
-        self.flag = flag
-        self.nation_name = nation_name
-        self.service_type = service_type
-        self.mode = mode
-        self.status = status
-        self.date_created = date_created
+
+@views.route('/create_nation', methods=['POST'])
+@login_required
+def create_nation():
+    form = nation_form_init()
+    if request.method == 'POST':
+        if form.validate_on_submit():
+            nation_exists = Nation.query.filter_by(nation_name=form.nation_name.data).first()
+            if nation_exists:
+                flash('A nation with this name already exists in NSRegent!', category='error')
+            else:
+                try:
+                    new_nation = Nation(nation_name=form.nation_name.data, nation_password=form.nation_password.data, service_type=form.service_type.data, mode=form.mode.data, user_id=current_user.id)
+                    db.session.add(new_nation)
+                    db.session.commit()
+                    flash(f'Success, {form.nation_name.data} was added', category='success')
+                except Exception as e:
+                    flash(f'failure, {form.nation_name.data} was not added error: {e}', category='error')
+                    print(form.data)
+
+    return render_template("core/nation_table.html")
